830a62270f8222817280bdbb24ce3e84700bb4ab
bramvanderzee <bram.vdzee@gmail.com>
2019-12-04 10:19:25 +0100

    solved 4

---
+++ 4/4a.py
@@ -0,0 +1,29 @@
+lower = 353096
+upper = 843213
+
+def check(num, lower, upper):
+    numList = sorted([int(x) for x in str(num)])
+    for i, x in enumerate(numList):
+        if int(num[i]) != int(x):
+            return False
+    foundAdjacent = False
+    for i, x in enumerate(num):
+        if i == 5:
+            break
+        if num[i+1] == x:
+            foundAdjacent = True
+            break
+    if not foundAdjacent:
+        return False
+
+    if int(num) < lower or int(num) > upper:
+        return False
+
+    return True
+
+passwords = []
+for i in range(lower, upper+1):
+    if check(str(i), lower, upper):
+        passwords.append(i)
+
+print(len(passwords))
---
+++ 4/4b.py
@@ -0,0 +1,30 @@
+lower = 353096
+upper = 843213
+
+def check(num, lower, upper):
+    numList = sorted([int(x) for x in str(num)])
+    for i, x in enumerate(numList):
+        if int(num[i]) != int(x):
+            return False
+    foundAdjacent = False
+    for i, x in enumerate(num):
+        if i == 5:
+            break
+        if int(num[i+1]) == int(x) and num.count(x) == 2:
+            foundAdjacent = True
+            break
+    if not foundAdjacent:
+        return False
+
+    if int(num) < lower or int(num) > upper:
+        return False
+
+    return True
+
+passwords = []
+for i in range(lower, upper+1):
+    if check(str(i), lower, upper):
+        passwords.append(i)
+
+print(passwords)
+print(len(passwords))
